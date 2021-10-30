5605dc69b425a354f2c7e3429c0b86500ca1a809
Daniela Paiva <danielapv200@gmail.com>
2021-10-30 11:41:33 -0500

    api

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@
     //   path: 'https://cclam.org.pe/feriavirtual',
     // },
     env: {
-      // NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
+      NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
       // NEXT_BK_WS:"http://localhost:8000/api/"
     },
   }
