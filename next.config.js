ff7a017920737b8751ade43c377f2ff71ae48100
Daniela Paiva <danielapv200@gmail.com>
2021-11-05 21:53:10 -0500

    vercel prod

--- next.config.js
+++ next.config.js
@@ -9,27 +9,27 @@
 //     },
 //   }
 
-//  const nextConfig = {
-//     basePath: '/feriavirtual',
-//     images: {
-//       domains: ['www.cclam.org.pe', 'localhost','www.tailwindui.com'],
-//       loader: 'imgix',
-//       path: 'https://cclam.org.pe/feriavirtual',
-//     },
-//     env: {
-//       NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
-//     },
-//   }
-
-  const nextConfig = {
+ const nextConfig = {
+    basePath: '/feriavirtual',
     images: {
-      domains: ['localhost','www.tailwindui.com','http://www.bicentenariobancoseguro.com.s3-website-sa-east-1.amazonaws.com/'],
+      domains: ['www.cclam.org.pe', 'localhost','www.tailwindui.com'],
       loader: 'imgix',
-      path: 'http://www.bicentenariobancoseguro.com.s3-website-sa-east-1.amazonaws.com/',
+      path: 'https://cclam.org.pe/feriavirtual',
     },
     env: {
       NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
     },
   }
+
+  // const nextConfig = {
+  //   images: {
+  //     domains: ['localhost','www.tailwindui.com','http://www.bicentenariobancoseguro.com.s3-website-sa-east-1.amazonaws.com/'],
+  //     loader: 'imgix',
+  //     path: 'http://www.bicentenariobancoseguro.com.s3-website-sa-east-1.amazonaws.com/',
+  //   },
+  //   env: {
+  //     NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
+  //   },
+  // }
   
   module.exports = nextConfig
