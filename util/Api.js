b5babb894a6bd360ca4d8fc3a9f033dfe6b670b8
Daniela Paiva <danielapv200@gmail.com>
2021-11-15 22:09:39 -0500

    urgente

--- util/Api.js
+++ util/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api/",
-  // baseURL: "https://cclam.org.pe/recursos.feriavirtual/public/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "https://cclam.org.pe/recursos.feriavirtual/public/api/",
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
