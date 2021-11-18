77c08d7b12f0eea81efaf2be2bacd1f06374a6a7
Daniela Paiva <danielapv200@gmail.com>
2021-11-18 13:46:51 -0500

    URGENTE CONFIG

--- next.config.js
+++ next.config.js
@@ -3,33 +3,33 @@
  */
 
 
-//  const nextConfig = {
-//     env: {
-//       NEXT_BK_WS:"http://localhost:8000/api/"
-//     },
-//   }
-
  const nextConfig = {
-    basePath: '/feriaprueba',
-    images: {
-      domains: ['www.cclam.org.pe', 'localhost','www.tailwindui.com'],
-      loader: 'imgix',
-      path: 'https://cclam.org.pe/feriaprueba',
-    },
     env: {
-      NEXT_BK_WS:"https://cclam.org.pe/recursos.feriaprueba/public/api/"
+      NEXT_BK_WS:"http://localhost:8000/api/"
     },
   }
 
-  // const nextConfig = {
-  //   images: {
-  //     domains: ['localhost','www.tailwindui.com','https://feriabancaseguros.com/'],
-  //     loader: 'imgix',
-  //     path: 'https://feriabancaseguros.com/',
-  //   },
-  //   env: {
-  //     NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
-  //   },
-  // }
+//  const nextConfig = {
+//     basePath: '/feriaprueba',
+//     images: {
+//       domains: ['www.cclam.org.pe', 'localhost','www.tailwindui.com'],
+//       loader: 'imgix',
+//       path: 'https://cclam.org.pe/feriaprueba',
+//     },
+//     env: {
+//       NEXT_BK_WS:"https://cclam.org.pe/recursos.feriaprueba/public/api/"
+//     },
+//   }
+
+  const nextConfig = {
+    images: {
+      domains: ['localhost','www.tailwindui.com','https://feriabancaseguros.com/'],
+      loader: 'imgix',
+      path: 'https://feriabancaseguros.com/',
+    },
+    env: {
+      NEXT_BK_WS:"https://cclam.org.pe/recursos.feriavirtual/public/api/"
+    },
+  }
   
   module.exports = nextConfig
